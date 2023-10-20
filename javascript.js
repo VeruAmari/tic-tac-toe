c22374926192093e13452c274aa878517f9cf37d
Veru <43966969+VeruAmari@users.noreply.github.com>
2023-10-20 12:06:06 -0300

    Implement gameboard-module and player-factory

--- javascript.js
+++ javascript.js
@@ -1,1 +1,35 @@
-console.log("Hello World")
+const Gameboard = (function () {
+    const board = ["", "", "", "", "", "", "", "", ""];
+
+    const getBoard = function () {
+        console.log("Fetching board status");
+        return board
+    };
+
+    const updateBoard = function (square, mark) {
+        console.log("Placing an " + mark + " mark.");
+        board[square] = mark;
+    };
+
+    const restart = function () {
+        console.log("Restarting match.");
+        for (square in board) {
+            square = "";
+        }
+    };
+
+    return ({ getBoard, updateBoard, restart })
+})();
+
+const newPlayer = function (name, mark) {
+    const playerName = name;
+    const playerMark = mark;
+
+    const getName = function () {
+        return playerName
+    };
+    const getMark = function () {
+        return playerMark
+    };
+    return ({getName, getMark})
+}
